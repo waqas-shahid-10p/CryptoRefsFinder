f642762b18df5033a762f3afbcda1b92cf0a437e
Waqas Shahid <waqas.shahid@tenpearls.com>
2017-04-10 20:23:23 +0500

    minor changes

--- src/main/java/com/tenpearls/docker/crypto/Main.java
+++ src/main/java/com/tenpearls/docker/crypto/Main.java
@@ -2,23 +2,18 @@ package com.tenpearls.docker.crypto;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
-import org.apache.poi.xssf.usermodel.XSSFCellStyle;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.*;
-import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Main {
-    private final static Pattern CRYPTO_IMPORT = Pattern.compile("\"crypto/([A-za-z0-9]+)\"");
+    private final static Pattern CRYPTO_IMPORT = Pattern.compile("crypto((/([A-Za-z0-9]+))*)");
     private final static String USAGE_PATTERN = "(%s)\\.([A-Za-z0-9_])+";
 
     private static class Options {
@@ -27,7 +22,8 @@ public class Main {
 
         private Options() {
             if (System.getProperty("path") == null) {
-                this.parentPath = System.getProperty("user.dir");
+                //this.parentPath = System.getProperty("user.dir");
+                this.parentPath = "/home/waqas/go/src/github.com/docker/orca";
             } else {
                 this.parentPath = System.getProperty("path");
             }
@@ -49,8 +45,8 @@ public class Main {
     public static void main(final String[] args) throws IOException {
         final Options options = new Options();
         final Collection<File> files = FileUtils.listFiles(new File(options.parentPath), new String[]{"go"}, true);
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("Results");
+        Workbook workbook = new HSSFWorkbook();
+        Sheet sheet = workbook.createSheet("Results");
         final Font font = workbook.createFont();
         font.setBold(true);
         final CellStyle cellStyle = workbook.createCellStyle();
@@ -80,12 +76,13 @@ public class Main {
                 Pattern pattern = null;
                 int lineNum = 1;
                 boolean firstMatch = false;
+                final Map<String,String> algoFullName = new HashMap<>();
                 while (lineIterator.hasNext()) {
                     final String line = lineIterator.nextLine();
                     Matcher matcher = CRYPTO_IMPORT.matcher(line);
                     if (matcher.find()) {
-                        fileMatches.algorithms.add(matcher.group(1));
-                        pattern = Pattern.compile(String.format(USAGE_PATTERN, String.join("|", fileMatches.algorithms)));
+                        algoFullName.put(matcher.group(matcher.groupCount()),matcher.group(1));
+                        pattern = Pattern.compile(String.format(USAGE_PATTERN, String.join("|", algoFullName.keySet())));
                         if (!firstMatch) {
                             row = sheet.createRow(rowNum++);
                             firstMatch = true;
@@ -98,10 +95,10 @@ public class Main {
                             cell.setCellValue(file.getName());
                             cell.getCellStyle().setWrapText(true);
                             cell = row.createCell(1, CellType.STRING);
-                            cell.setCellValue(file.getPath());
+                            cell.setCellValue(file.getPath().replaceFirst(options.parentPath, ""));
                             cell.getCellStyle().setWrapText(true);
                             cell = row.createCell(2, CellType.STRING);
-                            cell.setCellValue(matcher.group(1));
+                            cell.setCellValue(algoFullName.get(matcher.group(1)));
                             cell.getCellStyle().setWrapText(true);
                             cell = row.createCell(3, CellType.STRING);
                             cell.setCellValue(matcher.group(0));
@@ -113,6 +110,10 @@ public class Main {
                     }
                     lineNum++;
                 }
+                if(firstMatch && !row.cellIterator().hasNext()){
+                    sheet.removeRow(row);
+                    rowNum--;
+                }
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -120,9 +121,9 @@ public class Main {
         for (int i = 0; i <= sheet.getLastRowNum(); i++) {
             sheet.autoSizeColumn(i);
         }
-        FileOutputStream sheetFile = new FileOutputStream(new File("References.xslx"));
+        FileOutputStream sheetFile = new FileOutputStream(new File("References.xls"));
         workbook.write(sheetFile);
         workbook.close();
-        System.out.println("References.xslx generated successfully");
+        System.out.println("References.xls generated successfully");
     }
 }
